cbebe4c79e10c8c9cd598f4d5195dbbb992d2a3e
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-11-19 14:56:33 -0500

    Added json request

--- controller.js
+++ controller.js
@@ -14,9 +14,22 @@ var myApp = angular.module('myApp', ['ngRoute']);
 		});
 	});
 
-myApp.controller('trumpTweets',function ($scope, $location){
+myApp.controller('trumpTweets',function ($scope, $location, $http){
+
+	$scope.getData = function(){
+		var url ="http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=trump&secondHash=women";
+		
+		$http.get(url).success(function(data){
+			
+			console.log(data);
+			// tweets = tweetData.query.results;
+			
+		});
+		console.log(data);
+	}
 
 	$scope.secondPage = function(){
 		$location.path('load-tweets.html')
 	}
+
 });

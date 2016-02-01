63d1c4cb04c7582ddf0d6cedf2615ca45613d7e1
Oliver Lai <oliverlai55@gmail.com>
2016-01-31 19:36:32 -0500

    layout changes to the tweet page

---
+++ scripts.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+	var windowHeight = window.innerHeight;
+	$('#intro-page-wrapper').css('height', windowHeight + 'px');
+	var contentHeight = $('#intro-page').height();
+	var paddingAdjust = parseInt((windowHeight - contentHeight-25)/2);
+	$('#logo').css('padding-top', paddingAdjust + 'px');
+},10);
+
+function viewProject(){
+	document.getElementById("intro-page-wrapper").style.display="none";
+}
